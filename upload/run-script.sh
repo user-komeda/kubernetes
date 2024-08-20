c2e6169f41a4ebc3dd5e9e18d52370b2a53e6874
ks <ks@localhost.localdomain>
2024-08-20 20:00:14 +0900

    #12 kubenetes_hard_way_vagrant_chapter6

--- upload/run-script.sh
+++ upload/run-script.sh
@@ -5,10 +5,19 @@ sudo chmod +x ./certificates-settings.sh
 sudo chmod +x ./cert_verify.sh
 sudo chmod +x ./generate-kubeconfig.sh
 sudo chmod +x ./data-encryption-config.sh
+sudo chmod +x ./etcd-cluster-settings.sh
+sudo chmod +x ./start-etcd.sh
 
 ./ssh-key.sh
 ./install-kubectl.sh
 ./certificates-settings.sh
 ./generate-kubeconfig.sh
 ./data-encryption-config.sh
+scp -o StrictHostKeyChecking=no ./etcd-cluster-settings.sh controlplane01:~/
+scp -o StrictHostKeyChecking=no ./etcd-cluster-settings.sh controlplane02:~/
+ssh controlplane01  ./etcd-cluster-settings.sh
+ssh controlplane02  ./etcd-cluster-settings.sh
+#ssh controlplane02 sh ./etcd-cluster-settings.sh  && sh ./start-etcd.sh
+#ssh controlplane01 sh ./start-etcd.sh 
+#ssh controlplane02 sh ./start-etcd.sh 
 #./cert_verify.sh
